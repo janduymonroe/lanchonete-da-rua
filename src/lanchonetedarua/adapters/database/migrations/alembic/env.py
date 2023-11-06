aff1df00b5ad84f2755aac655b4ece3a5a874685
Janduy Monroe <66971685+janduymonroe@users.noreply.github.com>
2023-11-06 12:30:36 -0300

    Update env.py

--- src/lanchonetedarua/adapters/database/migrations/alembic/env.py
+++ src/lanchonetedarua/adapters/database/migrations/alembic/env.py
@@ -4,11 +4,12 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from lanchonetedarua.configuration import Config
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('sqlalchemy.url', Settings().DATABASE_URL)
+config.set_main_option('sqlalchemy.url', Config().DATABASE_URL)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
