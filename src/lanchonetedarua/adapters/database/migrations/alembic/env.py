7ba18f3f636b5111fe4da83ae943f04f7800f917
Janduy Monroe <66971685+janduymonroe@users.noreply.github.com>
2023-11-06 12:25:53 -0300

    Update env.py

--- src/lanchonetedarua/adapters/database/migrations/alembic/env.py
+++ src/lanchonetedarua/adapters/database/migrations/alembic/env.py
@@ -8,6 +8,12 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option('sqlalchemy.url', Settings().DATABASE_URL)
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
